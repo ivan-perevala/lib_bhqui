c068e4629e95cffe4413b20ca788f1ec1ff07a87
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-04 13:19:41 +0200

    Added subtype option for different progressbars

--- src/bhqui/_progress.py
+++ src/bhqui/_progress.py
@@ -9,7 +9,7 @@ from typing import Generator
 import bpy
 from bl_ui import space_statusbar
 from bpy.types import Context, PropertyGroup, STATUSBAR_HT_header, UILayout, WindowManager
-from bpy.props import StringProperty, FloatProperty, BoolProperty, IntProperty, CollectionProperty
+from bpy.props import StringProperty, EnumProperty, BoolProperty, IntProperty, CollectionProperty
 
 from . _unique_name import eval_unique_name
 
@@ -18,105 +18,78 @@ __all__ = (
 )
 
 
-def _update_statusbar():
-    bpy.context.workspace.status_text_set(text=None)
-
-
 class progress:
     """
     Displays progressbar in statusbar.
 
     :cvar int PROGRESS_BAR_UI_UNITS: UI units [4...12] for each progressbar (label and icon size does not count). 6 by default (readonly).
-
-    .. versionadded:: 3.3
     """
 
     _is_drawn = False
     _attrname = ""
 
+    @staticmethod
+    def _update_statusbar():
+        if bpy.context.workspace:
+            bpy.context.workspace.status_text_set(text=None)
+
     class ProgressPropertyItem(PropertyGroup):
-        """
-        Single progressbar indicator.
-        """
+        "Single progressbar indicator."
 
         identifier: StringProperty(
             maxlen=64,
             options={'HIDDEN'},
         )
-        """
-        Progressbar identifier name.
-
-        .. versionadded:: 3.6
-        """
+        "Progressbar identifier name."
 
         def _common_value_update(self, context: Context) -> None:
-            _update_statusbar()
+            progress._update_statusbar()
+
+        subtype: EnumProperty(
+            items=(
+                ('PERCENTAGE', "Percentage", ""),
+                ('STEP', "Step", ""),
+                ('CONTINUOUS', "Continuous", "")
+            ),
+            default='PERCENTAGE',
+            options={'HIDDEN'},
+            update=_common_value_update,  # type: ignore
+        )
+        "Progressbar subtype."
 
         valid: BoolProperty(
             default=True,
-            update=_common_value_update,
+            update=_common_value_update,  # type: ignore
         )
+        "If progressbar is still valid."
+
+        label: StringProperty(
+            default="Progress",
+            options={'HIDDEN'},
+            update=_common_value_update,  # type: ignore
+        )
+        "Progressbar label."
 
         num_steps: IntProperty(
             min=1,
             default=1,
             subtype='UNSIGNED',
             options={'HIDDEN'},
-            update=_common_value_update,
+            update=_common_value_update,  # type: ignore
         )
-        """
-        Number of steps for progress to complete.
-
-        .. versionadded:: 3.3
-        """
+        "Number of steps."
 
         step: IntProperty(
             min=0,
             default=0,
             subtype='UNSIGNED',
             options={'HIDDEN'},
-            update=_common_value_update,
+            update=_common_value_update,  # type: ignore
         )
-        """
-        Current progress step.
-
-        .. versionadded:: 3.3
-        """
-
-        def _get_progress(self):
-            return (self.step / self.num_steps) * 100
-
-        def _set_progress(self, _value):
-            pass
-
-        value: FloatProperty(
-            min=0.0,
-            max=100.0,
-            precision=1,
-            get=_get_progress,
-            # set=_set_progress,
-            subtype='PERCENTAGE',
-            options={'HIDDEN'},
-        )
-        """
-        Evaluated progress, readonly.
-
-        .. versionadded:: 3.3
-        """
-
-        label: StringProperty(
-            default="Progress",
-            options={'HIDDEN'},
-            update=_common_value_update,
-        )
-        """
-        Progressbar label.
-
-        .. versionadded:: 3.3
-        """
+        "Current step."
 
     def _func_draw_progress(self, context: Context):
-        layout: UILayout = self.layout
+        layout: UILayout = self.layout  # type: ignore
 
         layout.use_property_split = True
         layout.use_property_decorate = False
@@ -125,16 +98,28 @@ class progress:
         layout.separator_spacer()
         layout.template_reports_banner()
 
+
         if hasattr(WindowManager, progress._attrname):
             layout.separator_spacer()
+            layout.template_running_jobs()
             for item in progress.valid_progress_items():
                 row = layout.row(align=True)
 
-                row.progress(text=item.label, type='RING', factor=item.value / 100)
-                item.step += 5
-                item.num_steps = 100
-                if item.step == 100:
-                    item.step = 0
+                factor = item.step / item.num_steps
+
+                match item.subtype:
+                    case 'PERCENTAGE':
+                        row.label(text=item.label)
+                        row.progress(type='BAR', text=f"{factor * 100} %", factor=factor)
+                    case 'STEP':
+                        row.label(text=item.label)
+                        row.progress(type='BAR', text=f"{item.step} / {item.num_steps}", factor=factor)
+                    case 'CONTINUOUS':
+                        row.progress(type='RING', text=item.label, factor=factor)
+                        item.num_steps = 25
+                        item.step += 1
+                        if item.step >= 25:
+                            item.step = 0
 
         layout.separator_spacer()
 
@@ -178,7 +163,7 @@ class progress:
                     CollectionProperty(type=progress.ProgressPropertyItem, options={'HIDDEN'})
                 )
                 STATUSBAR_HT_header.draw = cls._func_draw_progress
-                _update_statusbar()
+                cls._update_statusbar()
 
             cls._is_drawn = True
             ret: progress.ProgressPropertyItem = getattr(bpy.context.window_manager, cls._attrname).add()
@@ -215,6 +200,6 @@ class progress:
 
         importlib.reload(space_statusbar)
         STATUSBAR_HT_header.draw = space_statusbar.STATUSBAR_HT_header.draw
-        _update_statusbar()
+        cls._update_statusbar()
 
         cls._is_drawn = False
