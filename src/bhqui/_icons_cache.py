a4fc6ad8c472fc78f6b3958c832ded9c400bcab8
Ivan Perevala <ivan95perevala@gmail.com>
2025-05-10 02:47:36 +0200

    Refactor `IconsCache` to OOP paradigm

--- src/bhqui/_icons_cache.py
+++ src/bhqui/_icons_cache.py
@@ -5,7 +5,6 @@
 from __future__ import annotations
 
 import os
-from typing import Iterable
 
 import bpy
 import bpy.utils.previews
@@ -21,78 +20,63 @@ class IconsCache:
     Abstract icons cache class.
     """
 
-    _directory: str = ""
-    "Icons source directory"
+    __slots__ = (
+        "_directory",
+        "_data_identifiers",
+        "_image_names",
+        "_cache",
+        "_pcoll_cache",
+    )
 
-    _cache: dict[str, int] = dict()
+    _directory: str
+    "Icons source directory."
+
+    _data_identifiers: tuple[str, ...]
+    "Icons data identifiers."
+
+    _image_names: tuple[str, ...]
+    "Icons image identifiers."
+
+    _cache: dict[str, int]
     "Icons map: `identifier: icon_value`"
-    
-    _pcoll_cache: None | bpy.utils.previews.ImagePreviewCollection = None
+
+    _pcoll_cache: None | bpy.utils.previews.ImagePreviewCollection
     "Image icons cache"
 
-    @classmethod
-    def _intern_initialize_from_data_files(cls, *, directory: str, ids: Iterable[str]):
-        for identifier in ids:
+    def _intern_initialize_from_data_files(self):
+        for identifier in self._data_identifiers:
             try:
-                icon_value = bpy.app.icons.new_triangles_from_file(os.path.join(directory, f"{identifier}.dat"))
+                icon_value = bpy.app.icons.new_triangles_from_file(os.path.join(self._directory, f"{identifier}.dat"))
             except ValueError:
                 # log.warning(f"Unable to load icon \"{identifier}\"")
                 icon_value = 0
 
-            cls._cache[identifier] = icon_value
+            self._cache[identifier] = icon_value
 
-    @classmethod
-    def _intern_initialize_from_image_files(cls, *, directory: str, ids: Iterable[str]):
-        pcoll = bpy.utils.previews.new()
-        for identifier in ids:
-            prv: ImagePreview = pcoll.load(identifier, os.path.join(directory, f"{identifier}.svg"), 'IMAGE')
-            cls._cache[identifier] = prv.icon_id
-        cls._pcoll_cache = pcoll
+    def _intern_initialize_from_image_files(self):
+        if self._image_names:
+            pcoll = bpy.utils.previews.new()
+            for name in self._image_names:
+                prv: ImagePreview = pcoll.load(name, os.path.join(self._directory, name), 'IMAGE')
+                self._cache[os.path.splitext(name)[0]] = prv.icon_id
+            self._pcoll_cache = pcoll
 
-    @classmethod
-    def initialize(cls, *, directory: str, data_identifiers: Iterable[str], image_identifiers: Iterable[str]):
-        """
-        Icons cache initialization method.
+    def __init__(self, *, directory: str = "", data_identifiers: tuple[str, ...] = (), image_names: tuple[str, ...] = ()):
+        self._directory = directory
+        self._data_identifiers = data_identifiers
+        self._image_names = image_names
+        self._cache = dict()
+        self._pcoll_cache = None
 
-        :param directory: Source icons directory. It may be images and data files.
-        :type directory: str
-        :param data_identifiers: Data file icons identifiers.
-        :type data_identifiers: Iterable[str]
-        :param image_identifiers: Image icon identifiers.
-        :type image_identifiers: Iterable[str]
-        """
+        self._intern_initialize_from_data_files()
+        self._intern_initialize_from_image_files()
 
-        if cls._cache and cls._directory == directory:
-            return
+    def release(self):
+        if self._pcoll_cache is not None:
+            bpy.utils.previews.remove(self._pcoll_cache)
+            self._pcoll_cache = None
 
-        cls.release()
+        self._cache.clear()
 
-        if directory:
-            cls._intern_initialize_from_data_files(directory=directory, ids=data_identifiers)
-            cls._intern_initialize_from_image_files(directory=directory, ids=image_identifiers)
-
-        cls._directory = directory
-
-    @classmethod
-    def release(cls):
-        """
-        Releases icons cache. Should be used in extension ``unregister`` function.
-        """
-
-        if cls._pcoll_cache is not None:
-            bpy.utils.previews.remove(cls._pcoll_cache)
-            cls._pcoll_cache = None
-
-        cls._cache.clear()
-
-    @classmethod
-    def get_id(cls, identifier: str) -> int:
-        """
-        Retrieves `icon_value` to be used in UI using icon identifier.
-
-        :param identifier: Icon identifier, one of provided during cache initialization.
-        :type identifier: str
-        :return: Icon value to be used for UI calls.
-        :rtype: int
-        """
-        return cls._cache.get(identifier, 0)
+    def get_id(self, identifier: str) -> int:
+        return self._cache.get(identifier, 0)
